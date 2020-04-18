c2fce61e83312f95093934bd9ea9b949727d56ea
hossamaltokhy <hossamaltokhy@gmail.com>
2020-04-18 16:17:57 +0200

    First Commit

---
+++ main.c
@@ -0,0 +1,24 @@
+/*
+ * File:   main.c
+ * Author: Hossam Eldeen Altokhy
+ *
+ * Created on April 18, 2020, 4:13 PM
+ * 
+ * 
+ * 
+ * 
+ * 
+git init   
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/hossamaltokhy/TrialRepo.git
+git push -u origin master
+ */
+
+
+#include <xc.h>
+
+int main(void) {
+    // Do Nothing
+    return 0;
+}
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=TrialRepo.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/TrialRepo.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=trialrepo.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/trialrepo.x.tar
